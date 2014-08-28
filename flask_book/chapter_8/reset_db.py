7a04b1840a9cc75a97bde9cad9eef7ea41d150ef
Dan <danandreist@gmail.com>
2014-08-28 17:29:23 +0300

    Delete reset_db.py

--- flask_book/chapter_8/reset_db.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-proc = subprocess.Popen(["rm -rf db.sqlite migrations/"], stdout=subprocess.PIPE, shell=True)
-(out, err) = proc.communicate()
-proc = subprocess.Popen(["python manage_db.py db init"], stdout=subprocess.PIPE, shell=True)
-(out, err) = proc.communicate()
-proc = subprocess.Popen(["python manage_db.py db migrate"], stdout=subprocess.PIPE, shell=True)
-(out, err) = proc.communicate()
-proc = subprocess.Popen(["python manage_db.py db upgrade"], stdout=subprocess.PIPE, shell=True)
-(out, err) = proc.communicate()
